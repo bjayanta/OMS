512e51a06259bf8abe1dc0e00156b346ae1c49f0
Jayanta Biswas <bjayanta.me@gmail.com>
2020-08-29 19:57:01 +0600

    Bug fixing at app.js file.

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const { request, response } = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
     response.send("Hello World!")
 })
 
-module.export = app
+module.exports = app
